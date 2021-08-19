45ef2d469bd54fcb46a1adc0a777518546a317e0
James Bromwell <943160+thw0rted@users.noreply.github.com>
2021-08-19 12:10:25 +0200

    Add test utils to test path mapping

--- src/getPoint.spec.ts
+++ src/getPoint.spec.ts
@@ -1,4 +1,8 @@
+import { TEST_INTRO } from "@testing";
 import { point } from "./getPoint";
+
+
+console.log("Point test loaded", TEST_INTRO);
 
 describe("GetPoint uses Esri REST types", () => {
     it("should have a point", () => {
--- src/sharepointIssue.spec.ts
+++ src/sharepointIssue.spec.ts
@@ -1,7 +1,10 @@
+import { add } from "@testing/util";
+
 import { checkWeb } from "./sharepointIssue";
 
 describe("Sharepoint connection", () => {
     it("should fail to get a parent", () => {
+        console.log("Two and two makes", add(2,2));
         return expectAsync(checkWeb("https://example.com/parent/sub")).toBeRejected();
     });
 });
